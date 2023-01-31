2e920c23fd349170c83cb4d083f3a46915b57984
VictorGasperi <victorgasperi@gmail.com>
2023-01-31 17:19:23 -0300

    Barra de progresso

--- ContaminationGame/Assets/Scripts/ProgressBarHandler.cs
+++ ContaminationGame/Assets/Scripts/ProgressBarHandler.cs
@@ -9,14 +9,19 @@ public class ProgressBarHandler : MonoBehaviour
 {
     // todo: Tentar finalizar a barra de progresso; Como adicionar um Canvas somente a um objeto em cena 
     [SerializeField] private NucleotideTileStorage nucleotideTileStorage;
-    [SerializeField] private NucleotidesGenerator nucleotidesGenerator;
+    //[SerializeField] private NucleotidesGenerator nucleotidesGenerator;
     [SerializeField] private AreaStateMachine areaStateMachine;
     [SerializeField] private AreaState initialAreaState;
     [SerializeField] private GameObject progressBarFill;
 
-    private void Update()
+    private void OnEnable()
     {
-        FillProgressBar();
+        nucleotideTileStorage.currentStorageChangedEvent.AddListener(FillProgressBar);
+    }
+
+    private void OnDisable()
+    {
+        nucleotideTileStorage.currentStorageChangedEvent.RemoveListener(FillProgressBar);
     }
 
     private void FillProgressBar()
@@ -25,12 +30,11 @@ public class ProgressBarHandler : MonoBehaviour
         {
             var currentNucleotides = nucleotideTileStorage.CurrentStorage;
             var maxNucleotides = nucleotideTileStorage.MaxStorage;
-            progressBarFill.transform.localScale = new Vector3(currentNucleotides / maxNucleotides -1, 0.2f, 1);
+            progressBarFill.transform.localScale = new Vector3(currentNucleotides / maxNucleotides - 0.15f , 0.2f, 1);
         }
         else
         {
             progressBarFill.transform.localScale = new Vector3(0, 0.2f, 1);
         }
-        
     }
 }
